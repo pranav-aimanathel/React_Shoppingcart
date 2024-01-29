46f76f5b414f3910e136a2b03768db0c65392e40
Pranav A L <pranaval2010@gmail.com>
2024-01-29 12:02:47 +0530

    Router and Context initiation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Header from './Components/Header'
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Cart from './Components/Cart';
 
 function App() {
   return (
-    <div className="App">
-     <Header />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Context from './Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Context>
+      <App />
+    </Context>
   </React.StrictMode>
 );
 
