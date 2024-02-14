a7457c30282210c407d568a0c8651a1a1c10f61e
Pranav A L <pranaval2010@gmail.com>
2024-02-14 12:33:39 +0530

    logic of products sorting

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,14 +6,48 @@ import Filter from './Filter';
 
 
 const Home = () => {
-const { state: {products}, } = CartState();
-console.log(products);
+const { 
+  state: {products}, 
+  productState: { sort, byStock, byFastDelivery, byRating, searchQuery },
+} = CartState();
+
+const transformProducts = () => {
+  let sortedProducts = products;
+
+  if (sort) {
+    sortedProducts = sortedProducts.sort((a,b) =>
+    sort === 'lowToHigh' ? a.price - b.price : b.price - a.price
+    );
+  }
+
+  if (!byStock) {
+    sortedProducts = sortedProducts.filter((prod) => prod.inStock)
+  }
+
+  if (byFastDelivery) {
+    sortedProducts = sortedProducts.filter((prod) => prod.fastDelivery)
+  }
+
+  if (byRating) {
+    sortedProducts = sortedProducts.filter(
+      (prod) => prod.ratings >= byRating
+    );
+  }
+
+  if (searchQuery) {
+    sortedProducts = sortedProducts.filter((prod) =>
+      prod.name.toLowerCase().includes(searchQuery)
+    );
+  }
+
+  return sortedProducts;
+};
 
   return (
     <div className='home'>
-      <Filter />
+      <Filter/>
       <div className='productContainer'>
-        {products.map((prod) => {
+        {transformProducts().map((prod) => {
           return <SingleProduct prod={prod} key={prod.id} />
         })}
       </div>
